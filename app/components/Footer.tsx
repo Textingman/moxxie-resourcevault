ca2c63371b42f321b717e01a90a9d4a0b875cd7d
Textingman <matt.morf@textvolt.com>
2026-07-01 18:42:39 -0400

    Initial site setup: Resource Flow

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -46,7 +46,7 @@ export default function Footer() {
           <div>
             <h4 className="font-semibold mb-4">Contact</h4>
             <p style={{ color: '#DDD6FE' }}>support@tryresourceflow.com</p>
-            <p className="mt-2" style={{ color: '#DDD6FE' }}>8 The Green, Ste A, Dover, DE 19901</p>
+            <p className="mt-2" style={{ color: '#DDD6FE' }}>4 Peddlers Row #1068, Newark, DE 19702</p>
             <p className="mt-2" style={{ color: '#DDD6FE' }}>(833) 571-9204</p>
           </div>
         </div>
